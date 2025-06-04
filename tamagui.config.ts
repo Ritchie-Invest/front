69b01c0d4d44a2fa9f4cf628d35eaab9ed52fea8
DFarau <doriane.farau@hetic.net>
2025-06-04 15:32:02 +0200

    feat(ui): setup Tamagui

---
+++ tamagui.config.ts
@@ -0,0 +1,11 @@
+import { createTamagui } from 'tamagui'
+import { config } from '@tamagui/config/v2'
+
+const tamaguiConfig = createTamagui(config)
+
+export type Conf = typeof tamaguiConfig
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends Conf {}
+}
+
+export default tamaguiConfig

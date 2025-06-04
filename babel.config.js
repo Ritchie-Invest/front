89575557e9a3593237196df601a6e7bd6512e11a
DFarau <doriane.farau@hetic.net>
2025-06-04 17:20:56 +0200

    chore: setup Tamagui

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['tamagui', 'react-native-reanimated/plugin'],
-  };
-};
+    plugins: [
+      ['module:@tamagui/babel-plugin', { config: './tamagui.config.ts' }],
+      'react-native-reanimated/plugin',
+    ],
+  }
+}

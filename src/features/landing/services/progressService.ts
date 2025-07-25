b5815e41b32dffd55b71f3a584b6a42eff6f4d80
Charles Lambret <79523370+CharlesLambret@users.noreply.github.com>
2025-07-25 22:15:02 +0200

    fix: fix progress hook 🐛
    * fix: fixed incompatibility errors between react and gradle and fixed cd workflows
    
    * chore: added jest to package.json
    
    * fix: .env not saved
    
    * fix: fixed progression service and hook errors caused by missing imports and missing typings

--- src/features/landing/services/progressService.ts
+++ src/features/landing/services/progressService.ts
@@ -1,10 +1,9 @@
 import { axiosInstance } from '../../../lib/api/axios';
-import { ChaptersAndLessonsResponse } from '../../landing/models/responses/chapter';
+import { ChaptersAndLessonsResponse } from '../models/responses/chapter';
 
 export const progressService = {
   async getUserProgress(): Promise<ChaptersAndLessonsResponse> {
     const response = await axiosInstance.get<ChaptersAndLessonsResponse>('/chapters/user/progress');
-
     return response.data;
   },
 };

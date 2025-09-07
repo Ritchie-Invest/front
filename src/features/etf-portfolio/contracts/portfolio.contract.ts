d737d71e7b619cd3263c072967fa9e90ea787da9
Charles Lambret <charleslambretpro@gmail.com>
2025-09-08 00:19:52 +0200

    feat: connect etf detail to backend

--- src/features/etf-portfolio/contracts/portfolio.contract.ts
+++ src/features/etf-portfolio/contracts/portfolio.contract.ts
@@ -2,6 +2,7 @@ import { Portfolio, PortfolioPosition } from '../models/portfolio';
 
 export interface PortfolioDataService {
   getPortfolio(): Promise<Portfolio>;
+  getPortfolioPositionByETF(etfId: string): Promise<PortfolioPosition>;
 }
 
 export interface PortfolioPositionProvider {
